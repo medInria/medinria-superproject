b2463a5b2b9143d8922dc4b41f71a94eb386c380
Olivier Commowick <olivier.commowick@inria.fr>
2013-02-25 13:50:18 +0100

    Adding specific case for Apple compilation flags (actually g++ should be enforced as a compiler too), removed previous ones that were making the app weird

--- cmake/ExternalProjectConfig.cmake
+++ cmake/ExternalProjectConfig.cmake
@@ -1,5 +1,10 @@
 set(ep_common_c_flags "${CMAKE_C_FLAGS_INIT} ${ADDITIONAL_C_FLAGS}")
-set(ep_common_cxx_flags "${CMAKE_CXX_FLAGS_INIT} ${ADDITIONAL_CXX_FLAGS} -fpermissive")
+
+if (NOT APPLE)
+    set(ep_common_cxx_flags "${CMAKE_CXX_FLAGS_INIT} ${ADDITIONAL_CXX_FLAGS} -fpermissive")
+else()
+    set(ep_common_cxx_flags "${CMAKE_CXX_FLAGS_INIT} ${ADDITIONAL_CXX_FLAGS}")
+endif()
 
 set(ep_common_cache_args
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
@@ -9,18 +14,6 @@ set(ep_common_cache_args
     -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
     -DBUILD_TESTING:BOOL=OFF
 )
-
-# Set CMake OSX variable to pass down the external projects
-
-set(CMAKE_OSX_EXTERNAL_PROJECT_ARGS)
-if (APPLE)
-    list(APPEND CMAKE_OSX_EXTERNAL_PROJECT_ARGS
-         -DCMAKE_OSX_ARCHITECTURES:STRING=${CMAKE_OSX_ARCHITECTURES}
-         -DCMAKE_OSX_SYSROOT:STRING=${CMAKE_OSX_SYSROOT}
-         -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=${CMAKE_OSX_DEPLOYMENT_TARGET}
-    )
-    list(APPEND ep_common_cache_args ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS})
-endif()
 
 # Compute -G arg for configuring external projects with the same CMake generator:
 
