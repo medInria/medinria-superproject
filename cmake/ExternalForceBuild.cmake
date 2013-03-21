f74ae0492a90bdca3a0fab58adb2212a6f2235d3
Florian Vichot <florian.vichot@inria.fr>
2013-03-21 20:09:47 +0100

    Forcebuild under Visual Studio was missing a configuration parameter

--- cmake/ExternalForceBuild.cmake
+++ cmake/ExternalForceBuild.cmake
@@ -2,7 +2,7 @@ function(ExternalForceBuild project)
 
     ExternalProject_Get_Property(${project} stamp_dir)
     ExternalProject_Add_Step(${project} forcebuild
-        COMMAND ${CMAKE_COMMAND} -E touch_nocreate "${stamp_dir}/${project}-configure"
+        COMMAND ${CMAKE_COMMAND} -E touch_nocreate "${stamp_dir}/${CMAKE_CFG_INTDIR}/${project}-configure"
         COMMENT "Forcing build step for '${project}'"
         DEPENDEES configure
         DEPENDERS build
