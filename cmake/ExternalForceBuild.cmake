30efc22b6b798192ab286f557dd51f9a52a9ead7
Florian Vichot <florian.vichot@inria.fr>
2013-03-22 15:48:05 +0100

    Change the forcebuild step slightly (should have the same effect)

--- cmake/ExternalForceBuild.cmake
+++ cmake/ExternalForceBuild.cmake
@@ -2,7 +2,7 @@ function(ExternalForceBuild project)
 
     ExternalProject_Get_Property(${project} stamp_dir)
     ExternalProject_Add_Step(${project} forcebuild
-        COMMAND ${CMAKE_COMMAND} -E touch_nocreate "${stamp_dir}/${CMAKE_CFG_INTDIR}/${project}-configure"
+        COMMAND ${CMAKE_COMMAND} -E remove "${stamp_dir}/${CMAKE_CFG_INTDIR}/${project}-build"
         COMMENT "Forcing build step for '${project}'"
         DEPENDEES configure
         DEPENDERS build
