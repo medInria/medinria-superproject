eec14e274bdf062c5a4803d9e122679a1b958524
René-paul Debroize <rene-paul.debroize@irisa.fr>
2013-05-13 15:34:50 +0200

    add diclaimer to CMakeLists.txt and .cmake files.

--- cmake/externals/Qt.cmake
+++ cmake/externals/Qt.cmake
@@ -1,3 +1,16 @@
+#######################################################################
+#
+# medInria
+#
+# Copyright (c) INRIA 2013. All rights reserved.
+# See LICENSE.txt for details.
+# 
+#  This software is distributed WITHOUT ANY WARRANTY; without even
+#  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  PURPOSE.
+#
+#######################################################################
+
 function(Qt_project)
 
     set(Qt-minvers 4.8.4 PARENT_SCOPE)
