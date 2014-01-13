13d131c3684f6283dff39e908b9814d01c7f5440
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-01-13 16:10:56 +0100

    Incorporate some suggestions.

--- cmake/externals/projects_modules/VTK.cmake
+++ cmake/externals/projects_modules/VTK.cmake
@@ -89,7 +89,7 @@ ExternalProject_Add(${ep}
   ${location}
   ${branch}
   UPDATE_COMMAND ""
-  PATCH_COMMAND patch -p1 -t < ${CMAKE_SOURCE_DIR}/Patchs/VTK.patch
+  PATCH_COMMAND patch -p1 -N < ${CMAKE_SOURCE_DIR}/patches/VTK.patch
   CMAKE_GENERATOR ${gen}
   CMAKE_ARGS ${cmake_args}
   DEPENDS ${${ep}_dependencies}
