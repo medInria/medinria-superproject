92e8fac9b005db6823fc31ea5351f714cbaea13f
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2015-06-29 17:32:52 +0200

    Initial dtk 1.0 setup (not cimpiling yet).

--- cmake/externals/projects_modules/dtk.cmake
+++ cmake/externals/projects_modules/dtk.cmake
@@ -2,7 +2,7 @@
 #
 # medInria
 #
-# Copyright (c) INRIA 2013. All rights reserved.
+# Copyright (c) INRIA 2013-2015. All rights reserved.
 # See LICENSE.txt for details.
 # 
 #  This software is distributed WITHOUT ANY WARRANTY; without even
@@ -12,112 +12,101 @@
 ###############################################################################
 
 function(dtk_project)
-set(ep dtk)
+    set(ep dtk)
 
+    ## #############################################################################
+    ## List the dependencies of the project
+    ## #############################################################################
 
-## #############################################################################
-## List the dependencies of the project
-## #############################################################################
+    list(APPEND ${ep}_dependencies Qt5)
+      
+    ## #############################################################################
+    ## Prepare the project
+    ## ############################################################################# 
 
-list(APPEND ${ep}_dependencies 
-  Qt4
-  )
-  
+    EP_Initialisation(${ep}  
+      USE_SYSTEM OFF 
+      BUILD_SHARED_LIBS ON
+      REQUIRED_FOR_PLUGINS ON
+      )
 
-## #############################################################################
-## Prepare the project
-## ############################################################################# 
+    if (NOT USE_SYSTEM_${ep})
+        ## #############################################################################
+        ## Set directories
+        ## #############################################################################
+          
+        EP_SetDirectories(${ep} 
+          EP_DIRECTORIES ep_dirs
+          )
 
-EP_Initialisation(${ep}  
-  USE_SYSTEM OFF 
-  BUILD_SHARED_LIBS ON
-  REQUIRED_FOR_PLUGINS ON
-  )
+        ## #############################################################################
+        ## Define repository where get the sources
+        ## #############################################################################
 
+        set(url git://github.com/d-tk/dtk.git)
+        if (NOT DEFINED ${ep}_SOURCE_DIR)
+          set(location GIT_REPOSITORY ${url})
+        endif()
 
-if (NOT USE_SYSTEM_${ep})
-## #############################################################################
-## Set directories
-## #############################################################################
-  
-EP_SetDirectories(${ep} 
-  EP_DIRECTORIES ep_dirs
-  )
+        ## #############################################################################
+        ## Add specific cmake arguments for configuration step of the project
+        ## #############################################################################
 
+        # set compilation flags
+        if (UNIX)
+          set(${ep}_c_flags "${${ep}_c_flags} -Wall")
+          set(${ep}_cxx_flags "${${ep}_cxx_flags} -Wall")
+        endif()
 
-## #############################################################################
-## Define repository where get the sources
-## #############################################################################
+        # Disable the dtk composer if QtDeclarative is missing.
+        include(CheckIncludeFileCXX)
+        set(CMAKE_REQUIRED_INCLUDES ${QT_INCLUDES})
+        Check_Include_File_CXX(QtDeclarative HasQtDeclarative)
 
-set(url git://dtk.inria.fr/+medinria/dtk/dtk-clone-medinria.git)
-if (NOT DEFINED ${ep}_SOURCE_DIR)
-  set(location GIT_REPOSITORY ${url})
-endif()
+        if (NOT HasQtDeclarative)
+          set(BUILD_DTK_COMPOSER OFF)
+        else()
+          set(BUILD_DTK_COMPOSER ON)  
+        endif()
 
+        set(cmake_args
+          ${ep_common_cache_args}
+          -DCMAKE_C_FLAGS:STRING=${${ep}_c_flags}
+          -DCMAKE_CXX_FLAGS:STRING=${${ep}_cxx_flags}   
+          -DCMAKE_SHARED_LINKER_FLAGS:STRING=${${ep}_shared_linker_flags}  
+          -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+          -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS_${ep}}
+          -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
+          -DDTK_BUILD_COMPOSER:BOOL=${BUILD_DTK_COMPOSER}
+          -DDTK_HAVE_NITE:BOOL=OFF
+          )
 
-## #############################################################################
-## Add specific cmake arguments for configuration step of the project
-## #############################################################################
+        ## #############################################################################
+        ## Add external-project
+        ## #############################################################################
 
-# set compilation flags
- if (UNIX)
-  set(${ep}_c_flags "${${ep}_c_flags} -Wall")
-  set(${ep}_cxx_flags "${${ep}_cxx_flags} -Wall")
-endif()
+        ExternalProject_Add(${ep}
+          ${location}
+          ${ep_dirs}
+          UPDATE_COMMAND ""
+          CMAKE_GENERATOR ${gen}
+          CMAKE_ARGS ${cmake_args}
+          DEPENDS ${${ep}_dependencies}
+          INSTALL_COMMAND ""      
+          )
 
-# Disable the dtk composer if QtDeclarative is missing.
-include(CheckIncludeFileCXX)
-set(CMAKE_REQUIRED_INCLUDES ${QT_INCLUDES})
-Check_Include_File_CXX(QtDeclarative HasQtDeclarative)
+        ## #############################################################################
+        ## Set variable to provide infos about the project
+        ## #############################################################################
 
-if (NOT HasQtDeclarative)
-  set(BUILD_DTK_COMPOSER OFF)
-else()
-  set(BUILD_DTK_COMPOSER ON)  
-endif()
+        ExternalProject_Get_Property(${ep} binary_dir)
+        set(${ep}_DIR ${binary_dir} PARENT_SCOPE)
 
-set(cmake_args
-  ${ep_common_cache_args}
-  -DCMAKE_C_FLAGS:STRING=${${ep}_c_flags}
-  -DCMAKE_CXX_FLAGS:STRING=${${ep}_cxx_flags}   
-  -DCMAKE_SHARED_LINKER_FLAGS:STRING=${${ep}_shared_linker_flags}  
-  -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-  -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS_${ep}}
-  -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
-  -DDTK_BUILD_COMPOSER:BOOL=${BUILD_DTK_COMPOSER}
-  -DDTK_HAVE_NITE:BOOL=OFF
-  )
+        ## #############################################################################
+        ## Add custom targets
+        ## #############################################################################
 
+        EP_AddCustomTargets(${ep})
 
-## #############################################################################
-## Add external-project
-## #############################################################################
-
-ExternalProject_Add(${ep}
-  ${location}
-  ${ep_dirs}
-  UPDATE_COMMAND ""
-  CMAKE_GENERATOR ${gen}
-  CMAKE_ARGS ${cmake_args}
-  DEPENDS ${${ep}_dependencies}
-  INSTALL_COMMAND ""      
-  )
-
-
-## #############################################################################
-## Set variable to provide infos about the project
-## #############################################################################
-
-ExternalProject_Get_Property(${ep} binary_dir)
-set(${ep}_DIR ${binary_dir} PARENT_SCOPE)
-
-
-## #############################################################################
-## Add custom targets
-## #############################################################################
-
-EP_AddCustomTargets(${ep})
-
-endif() #NOT USE_SYSTEM_ep
-
+    endif()
 endfunction()
