fd215db8616a28e022d878b5fe3d306d2f8a7455
michael buckingham <michael.buckingham@inria.fr>
2015-07-02 16:15:49 +0200

    Enable ccache by default (if installed)

--- cmake/environment/CheckEnvironment.cmake
+++ cmake/environment/CheckEnvironment.cmake
@@ -94,6 +94,14 @@ else()
   mark_as_advanced(SVN_BIN)
 endif()
 
+## #############################################################################
+## Look for CCACHE (optional)
+## #############################################################################
+find_program(CCACHE_FOUND ccache)
+if(CCACHE_FOUND)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+endif(CCACHE_FOUND)
 
 ## #############################################################################
 ## Test ssh access for github
